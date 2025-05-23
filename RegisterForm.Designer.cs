1e4cfd8fd4216aee93405c5a5cb29e069bb41685
jitendra <jitendrapandey5653@gmail.com>
2025-05-23 12:56:08 +1200

    Revert "Merge pull request #3 from ReignBeato/Heli_LogIn"

--- RegisterForm.Designer.cs
+++ RegisterForm.Designer.cs
@@ -28,14 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(310, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(306, 218);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // RegisterForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(685, 389);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "RegisterForm";
             this.Text = "Register Form";
             this.ResumeLayout(false);
@@ -43,5 +54,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
