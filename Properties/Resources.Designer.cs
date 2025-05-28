8ad3a6f6c6861b497175c0edc4b8dcb95f826dc7
Reign Beato <beato.reign2022@gmail.com>
2025-05-29 11:17:23 +1200

    Reign: Fixed the conflict in dashboard.

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -65,9 +65,7 @@ namespace JHRTeam_App.Properties {
         /// </summary>
         internal static System.Drawing.Bitmap pexels_vertex_holiday_137931561_15917025 {
             get {
-
                 object obj = ResourceManager.GetObject("pexels-vertex-holiday-137931561-15917025", resourceCulture);
-
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
@@ -75,11 +73,19 @@ namespace JHRTeam_App.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-
         internal static System.Drawing.Bitmap pexels_vertex_holiday_137931561_159170251 {
             get {
                 object obj = ResourceManager.GetObject("pexels-vertex-holiday-137931561-159170251", resourceCulture);
-
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Untitled_design__5_ {
+            get {
+                object obj = ResourceManager.GetObject("Untitled_design__5_", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
--- RegisterForm.cs
+++ RegisterForm.cs
@@ -120,4 +120,5 @@ namespace JHRTeam_App
             }
         }
     }
+}
 
