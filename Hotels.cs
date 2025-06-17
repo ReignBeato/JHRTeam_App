6851f7abef2c3bdc22a3aa49063fe77435d6382c
Helipatel-hub <helipatel297@gmail.com>
2025-06-17 20:42:40 +1200

    Heli: complete code for hotel. change font size

--- Hotels.cs
+++ Hotels.cs
@@ -15,18 +15,39 @@ namespace JHRTeam_App
         public Hotels()
         {
             InitializeComponent();
-
             // Load hotel images from resources
+            picboxEverestviewhotel.Image = Properties.Resources.hotelEverestview;
+            picboxBarahi.Image = Properties.Resources.barahi_hotel;
+            picboxGokarnaresort.Image = Properties.Resources.Gokarnaresort;
+            picboxMiddlepathhotel.Image = Properties.Resources.Middle_path_hotel;
+            picboxNaturalspringhotel.Image = Properties.Resources.natural_spring_hotel;
+            picboxPanormahotel.Image = Properties.Resources.Panorma_hotel;
+            picboxSambalahotel.Image = Properties.Resources.sambala_hotel;
+            picboxTerracesresort.Image = Properties.Resources.Terraces_resort;
+
+            // Set the size mode for each PictureBox to stretch the image
+            picboxEverestviewhotel.SizeMode = PictureBoxSizeMode.StretchImage;
+            picboxBarahi.SizeMode = PictureBoxSizeMode.StretchImage;
+            picboxGokarnaresort.SizeMode = PictureBoxSizeMode.StretchImage;
+            picboxMiddlepathhotel.SizeMode = PictureBoxSizeMode.StretchImage;
+            picboxNaturalspringhotel.SizeMode = PictureBoxSizeMode.StretchImage;
+            picboxPanormahotel.SizeMode = PictureBoxSizeMode.StretchImage;
+            picboxSambalahotel.SizeMode = PictureBoxSizeMode.StretchImage;
+            picboxTerracesresort.SizeMode = PictureBoxSizeMode.StretchImage;
+
+
 
 
         }
         private void Hotels_Load(object sender, EventArgs e)
         {
+         
+
 
         }
         private void buttonHotels_Click(object sender, EventArgs e)
         {
-
+          
         }
 
         private void buttonHome_Click(object sender, EventArgs e)
@@ -57,6 +78,44 @@ namespace JHRTeam_App
             LogInForm.ShowDialog();
         }
 
-     
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://hoteleverestview.com");
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://barahi.com");
+        }
+
+        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://gokarna.com");
+        }
+
+        private void linkLabel4_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.hotel-middlepath.com");
+        }
+
+        private void linkLabel5_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.hoteles.com/en/ho3073742016/hotel-natural-spring-pvt-ltd");
+        }
+
+        private void linkLabel6_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://panoramalodge.com.np");
+        }
+
+        private void linkLabel7_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://shambalahotel.com");
+        }
+
+        private void linkLabel8_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.terracesresort.com");
+        }
     }
 }
