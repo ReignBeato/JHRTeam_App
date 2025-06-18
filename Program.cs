856a025ae48ab9040abcc193d96d9875719d4890
Reign Beato <beato.reign2022@gmail.com>
2025-06-18 18:09:01 +1200

    Reign: Changed the application to run Dashboard first.

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace JHRTeam_App
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            Application.Run(new Tickets());
+            Application.Run(new DashboardForm());
         }
     }
 }
